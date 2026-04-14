ecef6f193b16b73ad5e322bef005e01f1385b22c
coopernelson112 <cooper.nelson.112@gmail.com>
2026-04-14 14:36:59 -0600

    Restore choose_var to main (#3)

---
+++ R/choose_var.R
@@ -0,0 +1,11 @@
+choose_var <- function(v1 = "ave_age_men", v2 = "ave_age_women",
+                       v3 = "nrr", v4 = "infantdeaths", v5 = "imr") {
+
+  preds <- vapply(c(v1, v2, v3, v4, v5), as.character, c(""))
+
+  user_data <- joined_data_1_ |>
+
+    select(year, country, preds[1], preds[2], preds[3], preds[4], preds[5])
+
+  user_data
+}
