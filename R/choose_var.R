38468f86dbde79bc2cebc56c98988bd673c08fd8
Cooper Nelson <coopernelson@Coopers-MacBook-Pro.local>
2026-04-20 15:05:08 -0600

    Last Edits

--- R/choose_var.R
+++ R/choose_var.R
@@ -1,20 +1,25 @@
-#' Choose any variables from retirement data to use for analysis
+#' Choose variables from retirement data to use for analysis
 #'
-#' If no variables are chosen, some general population statistics are kept
+#' If no variables are chosen, a default set of general population
+#' statistics is kept.
 #'
-#' @param ... Any variable chosen to keep in the final dataset
+#' @param ... Variable names to keep in the final dataset.
+#' @param data A data frame to select variables from. Defaults to `retirement`.
+#'
+#' @return A data frame containing only the selected variables.
 #' @export
 choose_var <- function(..., data = retirement) {
 
   chose <- c(...)
 
-    if (length(chose) == 0) {
-      chose <- c("country", "year", "PopDensity", "PopSexRatio",
-                  "MedianAgePop", "PopGrowthRate")
-    }
+  if (length(chose) == 0) {
+    chose <- c(
+      "country", "year", "PopDensity", "PopSexRatio",
+      "MedianAgePop", "PopGrowthRate"
+    )
+  }
 
-  fin_data <- retirement |>
-
+  fin_data <- data |>
     dplyr::select(dplyr::all_of(chose))
 
   return(fin_data)
