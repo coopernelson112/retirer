1dd940c09e46175cd5bf575bc7bb63975dd50227
AlKulAK <147559511+AlKulAK@users.noreply.github.com>
2026-04-26 14:21:46 -0600

    Update vignette (#6)
    * Add mapping functionality
    
    * Finalize package docs and remove raw data from tracking
    
    * Fix README
    
    * Fix README again
    
    * Update the vignette

--- tests/testthat/test-choose_var.R
+++ tests/testthat/test-choose_var.R
@@ -1,23 +1,18 @@
 test_that("correct variables chosen", {
   mt <- choose_var()
-  mt_test <- dplyr::select(retirement, country, year, PopDensity, PopSexRatio,
-                           MedianAgePop, PopGrowthRate)
+  mt_test <- dplyr::select(
+    retirement, country, year, PopDensity, PopSexRatio,
+    MedianAgePop, PopGrowthRate
+  )
 
   expect_equal(mt, mt_test)
 
   ns <- colnames(retirement)
 
-  for (i in seq(length(ns))){
-
+  for (i in seq_along(ns)) {
     tru_dat <- dplyr::select(retirement, ns[i])
     used_dat <- choose_var(ns[i])
 
     result <- expect_equal(tru_dat, used_dat)
-
   }
-
 })
-
-
-
-
